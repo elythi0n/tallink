a299159473818b2082294e92708e62ea10fdcfc9
Marcos Raudkett <info@marcosraudkett.com>
2018-12-20 18:33:59 +0200

    Create land-services.php

---
+++ tests/includes/land-services.php
@@ -0,0 +1,58 @@
+<div class="modal fade" id="landServices" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Land Services for <?php echo $_GET["sailId"]; ?></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <?php 
+
+          include("../../classes/Tallink.class.php");
+          $fetch_land_services = new Tallink();
+            $fetch_land_services->locale = 'en'; /* locale */
+            $fetch_land_services->country = ''; /* country */
+            $fetch_land_services->outwardSailId = $_GET["sailId"]; /* outwardSailId */
+          $fetch_land_services = Tallink::fetch_land_services($fetch_land_services); 
+
+          ?>  
+        <table class="table table-responsive">
+          <thead>
+            <th>Title</th>
+            <th style="white-space: nowrap;">Image</th>
+            <th>Description</th>
+            <th>Starting</th>
+            <th>Ending</th>
+            <th>Availability</th>
+            <th>Price</th>
+            <th>ClubOnePrice</th>
+          </thead>
+          <tbody>
+        <?php 
+        if($fetch_land_services != null) {
+                foreach($fetch_land_services as $row_service)
+                {
+                    ?>
+            <tr>
+            <td><?php echo $row_service["title"]; ?></td>
+            <td><img height="64" src="<?php echo $row_service["imageUrl"]; ?>"></td>
+            <td><?php echo $row_service["description"]; ?></td>
+            <td><?php foreach($row_service["eventTimes"] as $eventful) { echo $eventful['dateTime'];  ?></td>
+            <td><?php echo $eventful['endDateTime'];  ?></td>
+            <td><?php echo $eventful['hasPlaces'];  ?></td>
+            <td><?php echo $eventful['price'];  ?>€</td>
+            <td><?php echo $eventful['clubAccountPrice']; } ?>€</td>
+            </tr>
+            <?php }
+                }?>
+          </tbody>
+        </table>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
