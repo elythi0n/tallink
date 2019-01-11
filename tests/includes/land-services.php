6d6355b43b8c4884235619ab7462cdb96c7552bb
Marcos Raudkett <info@marcosraudkett.com>
2019-01-11 13:43:16 +0200

    Update land-services.php

--- tests/includes/land-services.php
+++ tests/includes/land-services.php
@@ -1,8 +1,12 @@
+<?php 
+if(isset($_GET["sailId"])) {
+  $sailId = $_GET['sailId'];
+?>
 <div class="modal fade" id="landServices" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">Land Services for <?php echo $_GET["sailId"]; ?></h5>
+        <h5 class="modal-title">Land Services for <?php echo $sailId; ?></h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -14,7 +18,7 @@
           $fetch_land_services = new Tallink();
             $fetch_land_services->locale = 'en'; /* locale */
             $fetch_land_services->country = ''; /* country */
-            $fetch_land_services->outwardSailId = $_GET["sailId"]; /* outwardSailId */
+            $fetch_land_services->outwardSailId = $sailId; /* outwardSailId */
           $fetch_land_services = Tallink::fetch_land_services($fetch_land_services); 
 
           ?>  
@@ -31,7 +35,10 @@
           </thead>
           <tbody>
         <?php 
-        if($fetch_land_services != null) {
+        if($fetch_land_services != null) 
+        {
+          if(is_array($fetch_land_services) || is_object($fetch_land_services)) 
+          {
                 foreach($fetch_land_services as $row_service)
                 {
                     ?>
@@ -45,8 +52,12 @@
             <td><?php echo $eventful['price'];  ?>€</td>
             <td><?php echo $eventful['clubAccountPrice']; } ?>€</td>
             </tr>
-            <?php }
-                }?>
+            <?php 
+                } /* foreach($fetch_land_services as $row_service) */
+            } /* if(is_array($fetch_land_services) || is_object($fetch_land_services))  */
+         } /* if($fetch_land_services != null) */
+            
+            ?>
           </tbody>
         </table>
       </div>
@@ -56,3 +67,4 @@
     </div>
   </div>
 </div>
+<?php } /* if(isset($_GET["sailId"])) */ ?>
